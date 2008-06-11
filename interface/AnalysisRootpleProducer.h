0eb7b4702331b8e055646be60a3bbf64c888675d
cmsbuild <>
2008-06-11 04:07:16 +0000

    This commit was manufactured by cvs2git to create tag 'QCDAnalysis- UEAnalysis-CMSSW_2_1_0'.
    Sprout from master 2008-05-18 21:17:55 UTC bechtel <sha1-ed5984511fca06e3b47a5f5254511ca13942a612@cern.ch> 'changes in accessing HLT bits; bugfix in root-macro'
    Cherrypick from master 2008-04-17 13:00:46 UTC bechtel <sha1-ed5984511fca06e3b47a5f5254511ca13942a612@cern.ch> 'updates for 2_0_0':
        data/UEAnalysisRootple.cfi
        data/UEAnalysisRootpleOnlyMC.cfi
        interface/AnalysisRootpleProducer.h
        interface/AnalysisRootpleProducerOnlyMC.h
        root/Makefile
        root/RunUE.sh
        root/UEAnalysis.cpp
        root/UEAnalysisOnRootple.C
        root/UEAnalysisOnRootple.h
        src/AnalysisRootpleProducer.cc
        src/AnalysisRootpleProducerOnlyMC.cc
        test/ueAnalysisRootFileChain.cfg
        test/ueAnalysisRootFileChainOnlyMC.cfg
        test/ueAnalysisRootFileChainOnlyReco.cfg
    Cherrypick from master 2008-06-11 04:07:15 UTC Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch> 'add missing translations':
        python/UEAnalysisJets_cfi.py
        python/UEAnalysisParticles_cfi.py
        python/UEAnalysisTracks_cfi.py

--- interface/AnalysisRootpleProducer.h
+++ interface/AnalysisRootpleProducer.h
@@ -58,6 +58,12 @@ public:
   virtual void endJob() ;
   
   void fillEventInfo(int);
+  void fillMCParticles(float, float, float, float);
+  void fillTracks(float, float, float, float);
+  void fillInclusiveJet(float, float, float, float);
+  void fillChargedJet(float, float, float, float);
+  void fillTracksJet(float, float, float, float);
+  void fillCaloJet(float, float, float, float);
   void store();
 
 private:
@@ -72,7 +78,6 @@ private:
   InputTag recoCaloJetCollName;
   InputTag tracksCollName;
   InputTag triggerResultsTag;
-  InputTag triggerEventTag;
 
   Handle< HepMCProduct        > EvtHandle ;
   Handle< vector<GenParticle> > CandHandleMC ;
@@ -82,7 +87,6 @@ private:
   Handle< BasicJetCollection  > TracksJetsHandle ;
   Handle< CaloJetCollection   > RecoCaloJetsHandle ;
   Handle<TriggerResults> triggerResults;
-  Handle<TriggerEvent>   triggerEvent;
   //  Handle<TriggerFilterObjectWithRefs> hltFilter; // not used at the moment: can access objects that fired the trigger
   TriggerNames triggerNames;
 
@@ -92,8 +96,22 @@ private:
 
   TTree* AnalysisTree;
 
-  int EventKind;
+  static const int NMCPMAX = 10000;   
+  static const int NTKMAX = 10000;
+  static const int NIJMAX = 10000;
+  static const int NCJMAX = 10000;
+  static const int NTJMAX = 10000;
+  static const int NEHJMAX = 10000;
+
+  int EventKind,NumberMCParticles,NumberTracks,NumberInclusiveJet,NumberChargedJet,NumberTracksJet,NumberCaloJet;
   
+  float MomentumMC[NMCPMAX],TransverseMomentumMC[NMCPMAX],EtaMC[NMCPMAX],PhiMC[NMCPMAX];
+  float MomentumTK[NTKMAX],TransverseMomentumTK[NTKMAX],EtaTK[NTKMAX],PhiTK[NTKMAX];
+  float MomentumIJ[NIJMAX],TransverseMomentumIJ[NIJMAX],EtaIJ[NIJMAX],PhiIJ[NIJMAX];
+  float MomentumCJ[NCJMAX],TransverseMomentumCJ[NCJMAX],EtaCJ[NCJMAX],PhiCJ[NCJMAX];
+  float MomentumTJ[NTJMAX],TransverseMomentumTJ[NTJMAX],EtaTJ[NTJMAX],PhiTJ[NTJMAX];
+  float MomentumEHJ[NEHJMAX],TransverseMomentumEHJ[NEHJMAX],EtaEHJ[NEHJMAX],PhiEHJ[NEHJMAX];
+
   TClonesArray* MonteCarlo;
   TClonesArray* InclusiveJet;
   TClonesArray* ChargedJet;
